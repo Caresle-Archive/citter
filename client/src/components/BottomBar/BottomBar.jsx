c7208eed5e527c08f64b11abf15a489255e43010
Caresle <carabesle@gmail.com>
2021-10-02 09:42:09 -0600

    Add bottom icons

--- client/src/components/BottomBar/BottomBar.jsx
+++ client/src/components/BottomBar/BottomBar.jsx
@@ -1,13 +1,19 @@
 import './BottomBar.css'
+import { 
+	HomeIcon,
+	MagnifyingGlassIcon,
+	BellIcon,
+	EnvelopeClosedIcon
+} from '@radix-ui/react-icons'
 
 const BottomBar = () => {
 	return (
 		<footer className="bottom-bar">
 			<ul>
-				<li>Home</li>
-				<li>Search</li>
-				<li>Notifications</li>
-				<li>Messages</li>
+				<li><HomeIcon className="icon-big"/></li>
+				<li><MagnifyingGlassIcon className="icon-big"/></li>
+				<li><BellIcon className="icon-big"/></li>
+				<li><EnvelopeClosedIcon className="icon-big"/></li>
 			</ul>
 		</footer>
 	)
